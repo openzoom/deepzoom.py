f8a8f1c1fff915a387f86e3495014c8ac5d53f21
Daniel Gasienica <daniel@gasienica.ch>
2011-10-06 14:50:05 +0200

    Fix style in `setup.py`.

--- setup.py
+++ setup.py
@@ -3,19 +3,21 @@
 
 from distutils.core import setup
 
-setup(name="DeepZoomTools",
-      version="0.9.4",
-      description="Python tools for generating Deep Zoom images (DZI) and collections (DZC) for the use with Silverlight Deep Zoom, Seadragon Ajax, Seadragon Mobile and OpenZoom.",
-      author="Daniel Gasienica",
-      author_email="daniel@gasienica.ch",
-      download_url="https://github.com/openzoom/deepzoom.py/archives/0.9.4",
-      keywords="deepzoom seadragon dzi dzc seadragonajax seadragonmobile silverlightdeepzoom microsoft openzoom",
-      url="http://github.com/openzoom/deepzoom.py",
-      py_modules=["deepzoom"],
-      classifiers=["Intended Audience :: Developers",
-                   "License :: OSI Approved :: BSD License",
-                   "Operating System :: OS Independent",
-                   "Programming Language :: Python",
-                   "Topic :: Utilities",
-                   "Topic :: Multimedia :: Graphics",
-                   "Topic :: Multimedia :: Graphics :: Graphics Conversion"])
+setup(name='DeepZoomTools',
+      version='0.9.4',
+      description='Python tools for generating Deep Zoom images (DZI) and \
+collections (DZC) for the use with Silverlight Deep Zoom, Seadragon Ajax, \
+Seadragon Mobile and OpenZoom.',
+      author='Daniel Gasienica',
+      author_email='daniel@gasienica.ch',
+      download_url='https://github.com/openzoom/deepzoom.py/archives/0.9.4',
+      keywords='deepzoom seadragon dzi dzc seadragonajax seadragonmobile silverlightdeepzoom microsoft openzoom',
+      url='http://github.com/openzoom/deepzoom.py',
+      py_modules=['deepzoom'],
+      classifiers=['Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities',
+                   'Topic :: Multimedia :: Graphics',
+                   'Topic :: Multimedia :: Graphics :: Graphics Conversion'])
