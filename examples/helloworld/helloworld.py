eb98ec08c1a2a627a6b1e711a6b23e3c3c571c09
Daniel Gasienica <daniel@gasienica.ch>
2020-01-16 12:25:41 -0500

    Update example

--- examples/helloworld/helloworld.py
+++ examples/helloworld/helloworld.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
 import deepzoom
 
 # Specify your source image
-SOURCE = "helloworld.jpg"
+SOURCE = f"file://{os.path.abspath('helloworld.jpg')}"
 
 # Create Deep Zoom Image creator with weird parameters
-creator = deepzoom.ImageCreator(tile_size=128, tile_overlap=2, tile_format="png",
-                                image_quality=0.8, resize_filter="bicubic")
+creator = deepzoom.ImageCreator(
+    tile_size=128,
+    tile_overlap=2,
+    tile_format="png",
+    image_quality=0.8,
+    resize_filter="bicubic",
+)
 
 # Create Deep Zoom image pyramid from source
 creator.create(SOURCE, "helloworld.dzi")
